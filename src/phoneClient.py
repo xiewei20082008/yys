f88ee56c005b41234f67c49732b1cdd8fc35bc19
Wei Xie <xiewei.fire@gmail.com>
2016-12-19 00:47:57 +0800

    修复手机运行 dropbox 异常，改用 spinner。 手机界面左右调整。增加识别准备率。发送指令增加确认功能。

--- src/phoneClient.py
+++ src/phoneClient.py
@@ -20,6 +20,7 @@ from kivy.uix.gridlayout import GridLayout
 from kivy.uix.scrollview import ScrollView
 from kivy.core.window import Window
 from kivy.lang import Builder
+from kivy.uix.spinner import Spinner
 
 Builder.load_string('''
 <ScrollableLabel>:
@@ -33,11 +34,25 @@ class ScrollableLabel(ScrollView):
     text = StringProperty('')
 
 times = 4
+def genSpinner(textList):
+    def show_selected_value(spinner, text):
+        print('The spinner', spinner, 'have text', text)
+    spinner = Spinner(
+        text=textList[0],
+        # available values
+        values=textList,
+        # just for positioning in our example
+        size_hint=(None, None),
+        size=(40*times, 25*times),
+        pos_hint={'center_x': .5, 'center_y': .5})
+    spinner.bind(text=show_selected_value)
+    return spinner
+
 class Client:
     def __init__(self):
         self.host = '172.246.84.119'
         self.port = 21567
-        self.bufsize = 1024
+        self.bufsize = 2000
         self.udpCliSock = socket(AF_INET, SOCK_DGRAM)
         self.addr = (self.host,self.port)
         print 'socket ok'
@@ -69,46 +84,63 @@ class phoneApp(App):
         def runUp():
             self.l_log.text = 'Command is under execution!'
             cmd = ' '.join([self.dd_hao.text,self.dd_start.text,self.dd_chapter.text,self.dd_aimEnergy.text,self.dd_rush.text])
+            # print 'real'
+            # print cmd
             ret = self.client.send('sendCmd:'+cmd)
             self.l_log.text = ret
-        t = Thread(target = runUp)
-        t.start()
+
+        if self.needConfirm:
+            cmd = ' '.join([self.dd_hao.text,self.dd_start.text,self.dd_chapter.text,self.dd_aimEnergy.text,self.dd_rush.text])
+            self.l_log.text = 'confirm cmd\ncmd'
+            self.needConfirm = False
+        else:
+            self.needConfirm = True
+            t = Thread(target = runUp)
+            t.start()
 
     def sendLog(self,instance):
         def runUp():
             self.l_log.text = 'Command is under execution!'
             ret = self.client.send('readLog')
             self.l_log.text = ret
+        self.needConfirm = True
         t = Thread(target = runUp)
         t.start()
 
     def build(self):
+        self.needConfirm = True
         self.client = Client()
         layout = BoxLayout(orientation='horizontal')
-        layoutLeft = BoxLayout(orientation='vertical')
+        layoutRight = BoxLayout(orientation='vertical')
 
         self.l_log = ScrollableLabel()
         # scollView = ScrollView(size_hint=(1, None), size=(Window.width, Window.height))
-        layout.add_widget(layoutLeft)
         layout.add_widget(self.l_log)
+        layout.add_widget(layoutRight)
         # self.l_log.text_size = scollView.width,None
 
         left_window_up = StackLayout()
         left_window_down = GridLayout(cols=2)
 
-        layoutLeft.add_widget(left_window_up)
-        layoutLeft.add_widget(left_window_down)
+        layoutRight.add_widget(left_window_up)
+        layoutRight.add_widget(left_window_down)
 
-        dropdown_hao = DropDown()
-        self.dd_hao = genDropDownButton(2,["dahao","xiaohao"],dropdown_hao)
-        dropdown_start = DropDown()
-        self.dd_start = genDropDownButton(2,["0","1"],dropdown_start)
-        dropdown_chapter = DropDown()
-        self.dd_chapter = genDropDownButton(2,["11","5"],dropdown_chapter)
-        dropdown_aimEnergy = DropDown()
-        self.dd_aimEnergy = genDropDownButton(2,["20","30"],dropdown_aimEnergy)
-        dropdown_rush = DropDown()
-        self.dd_rush = genDropDownButton(2,["True","False"],dropdown_rush)
+        # dropdown_hao = DropDown()
+        # self.dd_hao = genDropDownButton(2,["dahao","xiaohao"],dropdown_hao)
+        # dropdown_start = DropDown()
+        # self.dd_start = genDropDownButton(2,["0","1"],dropdown_start)
+        # dropdown_chapter = DropDown()
+        # self.dd_chapter = genDropDownButton(2,["11","5"],dropdown_chapter)
+        # dropdown_aimEnergy = DropDown()
+        # self.dd_aimEnergy = genDropDownButton(2,["20","30"],dropdown_aimEnergy)
+        # dropdown_rush = DropDown()
+        # self.dd_rush = genDropDownButton(2,["True","False"],dropdown_rush)
+
+        self.dd_hao = genSpinner(['dahao','xiaohao'])
+        self.dd_start = genSpinner(['0','1'])
+        self.dd_chapter = genSpinner(['11','5'])
+        self.dd_aimEnergy = genSpinner(['20','30'])
+        self.dd_rush = genSpinner(['True','False'])
 
 
         left_window_up.add_widget(self.dd_hao)
@@ -124,8 +156,8 @@ class phoneApp(App):
         b_exe = Button(text = 'execute')
         b_exe.bind(on_press=self.sendCmd)
 
-        left_window_down.add_widget(b_readLog)
         left_window_down.add_widget(b_exe)
+        left_window_down.add_widget(b_readLog)
 
         # layout.add_widget(dd_start)
         # left_window.add_widget(dd_hao)
